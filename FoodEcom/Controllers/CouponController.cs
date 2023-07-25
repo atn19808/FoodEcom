2f64161f30789326cf02a2fc9ef561fb636254a5
unknown <atn1908@outlook.com>
2023-07-25 17:40:05 -0500

    finish CRUD operations for Coupon

--- FoodEcom/Controllers/CouponController.cs
+++ FoodEcom/Controllers/CouponController.cs
@@ -2,6 +2,7 @@
 using FoodEcom.Web.Service.IService;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace FoodEcom.Web.Controllers
 {
@@ -23,6 +24,50 @@ namespace FoodEcom.Web.Controllers
                 list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
             }
             return View(list);
+        }
+
+        public async Task<IActionResult> CouponCreate()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> CouponDelete(int couponId)
+        {
+			ResponseDto? response = await _couponService.GetCouponByIdAsync(couponId);
+
+			if (response != null && response.IsSuccess)
+			{
+				CouponDto couponDto = JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(response.Result));
+				return View(couponDto);
+			}
+			return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponDelete(CouponDto couponDto)
+        {
+            ResponseDto? response = await _couponService.DeleteCouponAsync(couponDto.CouponId);
+
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction("CouponIndex");
+            }
+            return View(couponDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponCreate(CouponDto couponDto)
+        {
+            if (ModelState.IsValid)
+            {
+                ResponseDto? response = await _couponService.CreateCouponAsync(couponDto);
+
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction("CouponIndex");
+                }
+            }
+            return View(couponDto);
         }
     }
 }
--- FoodEcom/obj/Debug/net8.0/FoodEcom.Web.AssemblyInfo.cs
+++ FoodEcom/obj/Debug/net8.0/FoodEcom.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodEcom.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3a6c05159f9a528d8f3219f97398fb82d37d501")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodEcom.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodEcom.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
