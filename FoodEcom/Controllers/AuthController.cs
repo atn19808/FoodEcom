030a0be5c1f99eb35d60a8cc86dc60031b8da3df
unknown <atn1908@outlook.com>
2023-07-26 13:44:30 -0500

    finish adding login and register pages

---
+++ FoodEcom/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using FoodEcom.Web.Models;
+using FoodEcom.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodEcom.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDto loginRequestDto = new LoginRequestDto();
+            return View(loginRequestDto);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
--- FoodEcom/obj/Debug/net8.0/FoodEcom.Web.AssemblyInfo.cs
+++ FoodEcom/obj/Debug/net8.0/FoodEcom.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodEcom.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42924dc47b7b5d350c3a3c5e44f5f01688b799a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cdf17a8f61cccad1f873c262e066153ca21addb")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodEcom.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodEcom.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
