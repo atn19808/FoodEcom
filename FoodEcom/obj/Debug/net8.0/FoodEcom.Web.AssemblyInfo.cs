a389a94f9cb980d2f8618b348d7cd20887822479
unknown <atn1908@outlook.com>
2023-07-26 17:37:13 -0500

    added token provider

--- FoodEcom/obj/Debug/net8.0/FoodEcom.Web.AssemblyInfo.cs
+++ FoodEcom/obj/Debug/net8.0/FoodEcom.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodEcom.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8013205908dc50c5e71cfeefea9bfa26124220f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodEcom.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodEcom.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
