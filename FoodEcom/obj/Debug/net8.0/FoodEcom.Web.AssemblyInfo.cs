ec81ab33eac438626b8fa785d58e17fae28b0510
unknown <atn1908@outlook.com>
2023-07-26 23:13:09 -0500

    untrack

--- FoodEcom/obj/Debug/net8.0/FoodEcom.Web.AssemblyInfo.cs
+++ FoodEcom/obj/Debug/net8.0/FoodEcom.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodEcom.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93f9cec84763b30101a66de3ec637f4aac860a9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdb48a7c89ea5ae895c69cb5dc69c5904d6e9ea9")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodEcom.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodEcom.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
