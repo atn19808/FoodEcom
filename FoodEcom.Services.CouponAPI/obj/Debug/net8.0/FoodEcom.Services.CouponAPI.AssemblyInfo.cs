bdb48a7c89ea5ae895c69cb5dc69c5904d6e9ea9
unknown <atn1908@outlook.com>
2023-07-26 23:10:52 -0500

    rebase tables and modified gitignore

--- FoodEcom.Services.CouponAPI/obj/Debug/net8.0/FoodEcom.Services.CouponAPI.AssemblyInfo.cs
+++ FoodEcom.Services.CouponAPI/obj/Debug/net8.0/FoodEcom.Services.CouponAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodEcom.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93f9cec84763b30101a66de3ec637f4aac860a9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodEcom.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodEcom.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
