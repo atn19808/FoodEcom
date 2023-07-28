cb95ed8c7b202e16d923cba100ce0c20bd49e307
unknown <atn1908@outlook.com>
2023-07-28 18:10:35 -0500

    calling product API from Shopping Cart

--- FoodEcom/Program.cs
+++ FoodEcom/Program.cs
@@ -17,6 +17,7 @@ builder.Services.AddHttpClient<IProductService, ProductService>();
 SD.CouponAPIBase = builder.Configuration["ServiceUrls:CouponAPI"];
 SD.AuthAPIBase = builder.Configuration["ServiceUrls:AuthAPI"];
 SD.ProductAPIBase = builder.Configuration["ServiceUrls:ProductAPI"];
+SD.ShoppingCartAPIBase = builder.Configuration["ServiceUrls:ShoppingCartAPI"];
 
 builder.Services.AddScoped<ITokenProvider, TokenProvider>();
 builder.Services.AddScoped<IBaseService, BaseService>();
