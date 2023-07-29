c8fb6ac7bd2dcc74737d373d8c97ee575835472e
unknown <atn1908@outlook.com>
2023-07-28 19:11:58 -0500

    add cart service

---
+++ FoodEcom/Service/CartService.cs
@@ -0,0 +1,54 @@
+﻿using FoodEcom.Web.Models;
+using FoodEcom.Web.Service.IService;
+using FoodEcom.Web.Utility;
+
+namespace FoodEcom.Web.Service
+{
+    public class CartService : ICartService
+    {
+        private readonly IBaseService _baseService;
+
+        public CartService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+        public async Task<ResponseDto?> GetCartByUserIdAsync(string userId)
+        {
+            return await _baseService.SendAsync(new Models.RequestDto()
+            {
+                ApiType = SD.ApiType.GET,
+                Data = userId,
+                Url = SD.ShoppingCartAPIBase + "/api/cart/GetCart/" + userId
+            });
+        }
+
+        public async Task<ResponseDto?> ApplyCouponAsync(CartDto cartDto)
+        {
+            return await _baseService.SendAsync(new Models.RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = cartDto,
+                Url = SD.ShoppingCartAPIBase + "/api/cart/ApplyCoupon"
+            });
+        }
+        public async Task<ResponseDto?> RemoveCartAsync(int cartDetailsId)
+        {
+            return await _baseService.SendAsync(new Models.RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = cartDetailsId,
+                Url = SD.ShoppingCartAPIBase + "/api/cart/RemoveCart"
+            });
+        }
+
+        public async Task<ResponseDto?> CartUpsertAsync(CartDto cartDto)
+        {
+            return await _baseService.SendAsync(new Models.RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = cartDto,
+                Url = SD.ShoppingCartAPIBase + "/api/cart/CartUpsert"
+            }); 
+        }
+    }
+}
---
+++ FoodEcom/Service/IService/ICartService.cs
@@ -0,0 +1,12 @@
+﻿using FoodEcom.Web.Models;
+
+namespace FoodEcom.Web.Service.IService
+{
+    public interface ICartService
+    {
+        Task<ResponseDto?> GetCartByUserIdAsync(string userId);
+        Task<ResponseDto?> CartUpsertAsync(CartDto cartDto);
+        Task<ResponseDto?> RemoveCartAsync(int cartDetailsId);
+        Task<ResponseDto?> ApplyCouponAsync(CartDto cartDto);
+    }
+}
