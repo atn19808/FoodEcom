9867d180066211b23c4c342118441dc11a9d34a9
unknown <atn1908@outlook.com>
2023-08-04 01:43:50 -0500

    setup dbContext and tables for RewardAPI

---
+++ FoodEcom.Services.RewardAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using FoodEcom.Services.RewardAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodEcom.Services.RewardAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Rewards> Rewards { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
